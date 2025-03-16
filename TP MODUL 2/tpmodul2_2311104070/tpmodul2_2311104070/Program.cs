9a18e968dab5bd6cc0683553ffea60ef81026252
Alvin Bagus Firmansyah <rusmiantofajri@gmail.com>
2025-03-16 23:03:51 +0700

    first commit

---
+++ TP MODUL 2/tpmodul2_2311104070/tpmodul2_2311104070/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Bagian A: Menerima input satu karakter
+        Console.Write("Masukkan satu huruf: ");
+        char input = char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        if ("AIUEO".Contains(input))
+        {
+            Console.WriteLine($"Huruf {input} merupakan huruf vokal");
+        }
+        else
+        {
+            Console.WriteLine($"Huruf {input} merupakan huruf konsonan");
+        }
+
+        // Bagian B: Array bilangan genap
+        int[] bilanganGenap = { 2, 4, 6, 8, 10 };
+
+        for (int i = 0; i < bilanganGenap.Length; i++)
+        {
+            Console.WriteLine($"Angka genap {i + 1} : {bilanganGenap[i]}");
+        }
+    }
+}
